79b50405e5666e2b660e9cc4a1557d1f4e2a96cd
Jewel Hasan <jewel.hasan@cg-bd.com>
2025-04-09 09:22:23 +0600

    error debug

--- src/app/component/shared/Profile.jsx
+++ src/app/component/shared/Profile.jsx
@@ -1,8 +1,11 @@
-import { signOut } from 'next-auth/react';
+"use client"
+import { signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
 import React from 'react';
 
 const Profile = () => {
+    const session = useSession();
+    console.log(session.data.user.name)
     return (
         <div>
 <div className="dropdown dropdown-end">
@@ -18,12 +21,18 @@ const Profile = () => {
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
         <li>
           <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
+            <span className='font-bold'>User's Name:</span> {session.data.user.name}
+            
           </a>
         </li>
-        <li><a>Settings</a></li>
-        <li onClick={()=> signOut()}><a>Logout</a></li>
+        <li>
+          <a className="justify-between">
+            <span className='font-bold'>Email:</span> {session.data.user.email}
+            
+          </a>
+        </li>
+        {/* <li onClick={()=> signOut()}><a><button className='btn btn-sm btn-primary'>Logout</button></a></li> */}
+       <button onClick={()=> signOut()} className='btn btn-sm btn-primary'>Logout</button>
       </ul>
     </div>
   </div>
