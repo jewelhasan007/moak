bd64ecffac4235fedb7a829d8e9e19088d354172
Jewel Hasan <jewel.hasan@cg-bd.com>
2025-05-17 12:32:53 +0600

    final

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -10,7 +10,7 @@ const page = () => {
                 <div className='space-y-3'>
                     
                      <h1 className=' justify-around w-[550px] text-4xl text-pink-500 p-1 text-justify'>My Name is Mawsumi Akter.</h1>
-                     <p className=' lg:justify-around w-[550px] text-4xl text-pink-500 p-1 lg:text-justify'> I'm a Digital Marketer Based In Bangladesh And This Is A Selection Of My Poersonal And Professional Work.</p>
+                     <p className=' lg:justify-around w-[550px] text-4xl text-pink-500 p-1 md:text-justify'> I'm a Digital Marketer Based In Bangladesh And This Is A Selection Of My Poersonal And Professional Work.</p>
                 </div>
             
              <div className='p-2'>
@@ -25,7 +25,7 @@ const page = () => {
             
            </div>
            
-           <div className='lg:flex justify-around'>
+           <div className='md:flex md:justify-around'>
                  <p className='font-bold text-justify'>over the past 2 years, I’ve worked with a diverse range of clients, from startups to fortune 5 companies. I love to help businesses grow. I specialize in SEO, content marketing, and social media management, helping brands establish a strong online presence. My approach is all about blending creativity with analytics—optimizing content for both search engines and real people. Whether it’s boosting website traffic, improving conversion rates, or building brand authority, I thriveO on delivering results. Let’s connect and take your digital presence to the next level! </p>
             </div>
         </div>
