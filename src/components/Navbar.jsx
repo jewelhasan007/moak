730c2368741f2a1d329c73ac7cfef079158874ae
Jewel Hasan <jewel.hasan@cg-bd.com>
2025-10-04 16:48:29 +0600

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
   }, []);
 
   return (
-    <div className="navbar bg-gray-200 shadow-sm">
+    <div className="navbar bg-gray-200 shadow-sm fixed">
       <div className="navbar-start">
         <div className={` ${isDropdownOpen ? 'dropdown' : ''}`}>
           <div onClick={handleDropdownToggle} tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
